bc1cd757d432975399ecd9f95897d46fe6647c43
Ilnur Garifullin <ilnur.email@gmail.com>
2019-05-06 18:03:16 +0300

    Switch from shell script to python script in 04_deploy-to-stage stage

--- 04_deploy-to-stage/deploy.py
+++ 04_deploy-to-stage/deploy.py
@@ -2,24 +2,21 @@ import argparse
 import os
 from hydrosdk import sdk
 
-POSTFIX = 'stage'
 
 if __name__ == '__main__': 
     parser = argparse.ArgumentParser()
-    
     parser.add_argument('--model-version', required=True)
     parser.add_argument('--model-name', required=True)
     parser.add_argument('--hydrosphere-address', required=True)
+    parser.add_argument('--dev', help='Flag for development purposes', action="store_true")
     
     args = parser.parse_args()
-    arguments = args.__dict__
 
-    app_name = '{}-{}-app'.format(arguments['model_name'], POSTFIX)
-    with open('/stage-app-name.txt', 'w') as f:
-        f.write(app_name)
-    model = '{}:{}'.format(arguments['model_name'], arguments['model_version'])
+    app_name = '{}-stage-app'.format(args.model_name)
+    with open("./stage_app_name.txt" if args.dev else "/stage_app_name.txt", 'w') as file:
+        file.write(app_name)
 
+    model = sdk.Model.from_existing(args.model_name, args.model_version)
     application = sdk.Application.singular(app_name, model)
-
-    result = application.apply(arguments['hydrosphere_address'])
+    result = application.apply(args.hydrosphere_address)
     print(result)
